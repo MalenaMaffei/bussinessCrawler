613941ed9de25e5c92cdbd164050324b6f897e34
MalenaMaffei <malenamaffei@hotmail.com>
2017-10-09 19:38:56 -0400

    prueba logger funcionando

--- searchInput.py
+++ searchInput.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QLineEdit
 from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+ 
 import io
 import sitesScraper
 
@@ -22,14 +24,14 @@ class App(QWidget):
         search = self.getText()
         self.show()
 
-        stdout = sys.stdout
-        sys.stdout = io.StringIO()
-
-        sitesScraper.main(search)
-
-        output = sys.stdout.getvalue()
-        sys.stdout = stdout
-        print("stdout desde gui: " + output)
+        # stdout = sys.stdout
+        # sys.stdout = io.StringIO()
+        #
+        # sitesScraper.main(search)
+        #
+        # output = sys.stdout.getvalue()
+        # sys.stdout = stdout
+        # print("stdout desde gui: " + output)
 
 
 
@@ -38,10 +40,40 @@ class App(QWidget):
         if okPressed and text != '':
             return(text + " in Miami")
 
+class OutLog:
+    def __init__(self, edit, out=None, color=None):
+        """(edit, out=None, color=None) -> can write stdout, stderr to a
+        QTextEdit.
+        edit = QTextEdit
+        out = alternate stream ( can be the original sys.stdout )
+        color = alternate color (i.e. color stderr a different color)
+        """
+        self.edit = edit
+        self.out = None
+        self.color = color
 
+    def write(self, m):
+        if self.color:
+            tc = self.edit.textColor()
+            self.edit.setTextColor(self.color)
+
+        self.edit.moveCursor(QtGui.QTextCursor.End)
+        self.edit.insertPlainText( m )
+
+        if self.color:
+            self.edit.setTextColor(tc)
+
+        if self.out:
+            self.out.write(m)
 
 if __name__ == '__main__':
+
     app = QApplication(sys.argv)
+
+    sys.stdout = OutLog( edit, sys.stdout)
+    sys.stderr = OutLog( edit, sys.stderr, QtGui.QColor(255,0,0) )
+
+
     ex = App()
     sys.exit(app.exec_())
     # app.exec_()
